d1fd4e11415a68e1780a326c0d8b6d91ac01532a
Luc Grosheintz <luc.grosheintz@gmail.com>
2019-02-14 16:23:08 +0100

    Generate random doubles not floats.

--- benchmarks/zisa/math/poly2d.cpp
+++ benchmarks/zisa/math/poly2d.cpp
@@ -6,7 +6,7 @@
 static void bm_poly2d_eval(benchmark::State &state) {
   std::random_device rd;
   std::mt19937 e(rd());
-  std::uniform_real_distribution<float> dist(0, 1.0);
+  std::uniform_real_distribution<double> dist(0, 1.0);
 
   auto rand = [&e, &dist]() { return dist(e); };
 
