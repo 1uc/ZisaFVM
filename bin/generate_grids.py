5eca1e1db92c84458c5aed836718be7c75815b3c
Luc Grosheintz <luc.grosheintz@gmail.com>
2019-04-03 17:35:53 +0200

    Improve grid generation.

--- bin/generate_grids.py
+++ bin/generate_grids.py
@@ -25,6 +25,8 @@ def minimal_geo_files(path):
         path + "/convergence/unit_square_{:d}.geo".format(d) for d in range(4)
     ]
 
+def matching_geo_files(pattern):
+    return glob.glob("{}*.geo".format(pattern))
 
 def all_geo_files(path):
     return glob.glob("{}/*.geo".format(path)) + glob.glob("{}/**/*.geo".format(path))
@@ -37,13 +39,19 @@ if __name__ == "__main__":
                         action='store_true',
                         help="Generate only those grids required for testing.")
 
+    parser.add_argument("pattern",
+                        default=None,
+                        help="Generate only for the matching grids.")
+
     args = parser.parse_args()
 
     if args.minimal:
         geos = minimal_geo_files("grids")
+    elif args.pattern:
+        geos = matching_geo_files(args.pattern)
     else:
         geos = all_geo_files("grids")
 
     with multiprocessing.Pool() as pool:
         for i, geo in enumerate(pool.imap_unordered(run_gmesh, geos)):
-            print("[{:2d}/{}] {}".format(i, len(geos), geo))
+            print("[{:2d}/{}] {}".format(i+1, len(geos), geo))
