eb41e4bec0134d4a6d3273c78b23186041965033
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-01-12 11:38:19 +0100

    Implement a distributed CFL condition.

---
+++ include/zisa/model/distributed_cfl_condition.hpp
@@ -0,0 +1,25 @@
+#ifndef ZISA_DISTRIBUTED_CFL_CONDITION_HPP_CQPSI
+#define ZISA_DISTRIBUTED_CFL_CONDITION_HPP_CQPSI
+
+#include <zisa/config.hpp>
+#include <zisa/model/cfl_condition.hpp>
+#include <zisa/parallelization/all_reduce.hpp>
+
+namespace zisa {
+
+/// Compute on each part of the domain and then combine.
+class DistributedCFLCondition : public CFLCondition {
+public:
+  DistributedCFLCondition(std::shared_ptr<CFLCondition> cfl_condition,
+                          std::shared_ptr<AllReduce> all_reduce);
+
+  virtual double operator()(const AllVariables &u) override;
+
+private:
+  std::shared_ptr<CFLCondition> cfl_condition;
+  std::shared_ptr<AllReduce> all_reduce;
+};
+
+}
+
+#endif // ZISA_DISTRIBUTED_CFL_CONDITION_HPP
---
+++ src/zisa/model/distributed_cfl_condition.cpp
@@ -0,0 +1,16 @@
+#include <zisa/model/distributed_cfl_condition.hpp>
+
+namespace zisa {
+
+DistributedCFLCondition::DistributedCFLCondition(
+    std::shared_ptr<CFLCondition> cfl_condition,
+    std::shared_ptr<AllReduce> all_reduce)
+    : cfl_condition(std::move(cfl_condition)),
+      all_reduce(std::move(all_reduce)) {}
+
+double DistributedCFLCondition::operator()(const AllVariables &u) {
+  double dt_local = (*cfl_condition)(u);
+  return (*all_reduce)(dt_local);
+}
+
+}
