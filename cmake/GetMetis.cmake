3522a810a653a086268542544e10a878ebec619c
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-01-17 10:27:28 +0100

    Fix METIS cmake instructions.

--- cmake/GetMetis.cmake
+++ cmake/GetMetis.cmake
@@ -2,7 +2,7 @@ include("cmake/GetCompilerVersion.cmake")
 get_filename_component(C_COMPILER_NAME ${CMAKE_C_COMPILER} NAME)
 
 set(METIS_VER 5.1.0)
-set(METIS_DIR ${PROJECT_SOURCE_DIR}/metis-${METIS_VER}/${C_COMPILER_NAME}/${C_COMPILER_VERSION})
+set(METIS_DIR ${PROJECT_SOURCE_DIR}/third_party/metis-${METIS_VER}/${C_COMPILER_NAME}/${C_COMPILER_VERSION})
 
 add_library(metis INTERFACE)
 target_include_directories(metis INTERFACE ${METIS_DIR}/include)
