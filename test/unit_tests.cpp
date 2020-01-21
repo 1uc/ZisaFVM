b8e8cfecbf1d27f3f47d11f09b510433821817c1
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-01-21 13:41:47 +0100

    Add MPI initialization/finalization code to unit-tests.

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,2 +1,25 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "zisa/testing/testing_framework.hpp"
+
+#if ZISA_HAS_MPI == 1
+#include <zisa/parallelization/mpi.hpp>
+#endif
+
+int main( int argc, char* argv[] ) {
+#if ZISA_HAS_MPI == 1
+  int requested = MPI_THREAD_MULTIPLE;
+  int provided = -1;
+
+  MPI_Init_thread(&argc, &argv, requested, &provided);
+  LOG_ERR_IF(requested > provided,
+             "MPI does not support the requested level of multi-threading.");
+#endif
+
+  int result = Catch::Session().run( argc, argv );
+
+#if ZISA_HAS_MPI == 1
+  MPI_Finalize();
+#endif
+
+  return result;
+}
