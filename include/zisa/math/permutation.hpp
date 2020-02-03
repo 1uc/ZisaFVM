b1b617e9247ab647c0174aa9108a11fd2f427115
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-02-03 09:15:50 +0100

    Fix bug in permutations.

--- include/zisa/math/permutation.hpp
+++ include/zisa/math/permutation.hpp
@@ -50,40 +50,44 @@ Permutation factor_permutation(const array_const_view<int_t, 1> &sigma);
 template <class T, int n_dims>
 void apply_permutation(array_view<T, n_dims> data, const Permutation &sigma);
 
-
 // -- Implementation -----------------------------------------------------------
 template <class T>
 void apply_cycle(array_view<T, 1> &data, const Cycle &cycle) {
   T first = std::move(data[cycle[0]]);
 
   for (int_t i = 0; i < cycle.size() - 1; ++i) {
-    data[cycle[i]] = std::move(data[cycle[i+1]]);
+    data[cycle[i]] = std::move(data[cycle[i + 1]]);
   }
-  data[cycle[cycle.size()-1]] = std::move(first);
+  data[cycle[cycle.size() - 1]] = std::move(first);
 }
 
 template <class T>
 void apply_cycle(array_view<T, 2> &data, const Cycle &cycle) {
+  auto cycle_length = cycle.size();
   auto n_vars = data.shape(1);
 
   array<T, 1> first(n_vars);
-  for(int_t k = 0; k < n_vars; ++k) {
+  for (int_t k = 0; k < n_vars; ++k) {
     first(k) = std::move(data(cycle(0), k));
   }
 
-  for (int_t i = 0; i < cycle.size() - 1; ++i) {
-    for(int_t k = 0; k < data.shape(1); ++k) {
+  for (int_t i = 0; i < cycle_length - 1; ++i) {
+    for (int_t k = 0; k < n_vars; ++k) {
       data(cycle(i), k) = std::move(data(cycle(i + 1), k));
     }
   }
 
-  for(int_t k = 0; k < n_vars; ++k) {
-    data(cycle(n_vars-1), k) = std::move(first(k));
+  for (int_t k = 0; k < n_vars; ++k) {
+    data(cycle(cycle_length - 1), k) = std::move(first(k));
   }
 }
 
 template <class T, int n_dims>
 void apply_permutation(array_view<T, n_dims> data, const Permutation &sigma) {
+  if(data.shape(0) == 0) {
+    return;
+  }
+
   for (const auto &cycle : sigma.cycles) {
     apply_cycle(data, cycle);
   }
