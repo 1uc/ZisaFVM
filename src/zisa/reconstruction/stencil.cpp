6b98b4ba5d85506617524874d60baa95e73a40dd
Luc Grosheintz <luc.grosheintz@gmail.com>
2020-02-25 16:57:15 +0100

    Update the formula to compute the size of a stencil.

--- src/zisa/reconstruction/stencil.cpp
+++ src/zisa/reconstruction/stencil.cpp
@@ -149,8 +149,10 @@ int_t required_stencil_size(int deg, double factor, int n_dims) {
     return 1;
   }
 
-  // The polynomials being fitted all have zero mean.
-  return int_t(double(poly_dof(deg, n_dims) - 1) * factor);
+  // The actual polynomials being fitted have zero mean. Hence, one degree of
+  // freedom less. However, cell itself is included in the stencil. Hence we
+  // need one point more.
+  return int_t(double(poly_dof(deg, n_dims) - 1) * factor) + 1;
 }
 
 std::vector<int_t>
