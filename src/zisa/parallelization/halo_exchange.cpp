b32430e17cb6f1fe7aef89ed7fbf79e6201f780a
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-01-25 18:02:15 +0100

    Make compiler happy.

--- src/zisa/parallelization/halo_exchange.cpp
+++ src/zisa/parallelization/halo_exchange.cpp
@@ -1,6 +1,6 @@
 #include <zisa/parallelization/halo_exchange.hpp>
 
 namespace zisa {
-void NoHaloExchange::operator()(AllVariables &all_vars) { return; }
+void NoHaloExchange::operator()(AllVariables &) { return; }
 void NoHaloExchange::wait() { return; }
 }
