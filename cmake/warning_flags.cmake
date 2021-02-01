9c93fa63f01a64e37cc40c9d3d59eb003a77d953
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-01 17:00:59 +0100

    Add prefix to `warning_flags`.

--- cmake/warning_flags.cmake
+++ cmake/warning_flags.cmake
@@ -1,20 +1,21 @@
 # -- Flags ---------------------------------------------------------------------
+add_library(fvm_warning_flags INTERFACE)
 if(NOT TARGET warning_flags)
-  add_library(warning_flags INTERFACE)
-
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    target_compile_options(warning_flags INTERFACE -Wall)
-    target_compile_options(warning_flags INTERFACE -Wextra)
-    target_compile_options(warning_flags INTERFACE -Wconversion)
+    target_compile_options(fvm_warning_flags INTERFACE -Wall)
+    target_compile_options(fvm_warning_flags INTERFACE -Wextra)
+    target_compile_options(fvm_warning_flags INTERFACE -Wconversion)
   endif()
 
   if(ZISA_MAX_ERRORS)
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-      target_compile_options(warning_flags INTERFACE -fmax-errors=${ZISA_MAX_ERRORS})
+      target_compile_options(fvm_warning_flags INTERFACE -fmax-errors=${ZISA_MAX_ERRORS})
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-      target_compile_options(warning_flags INTERFACE -ferror-limit=${ZISA_MAX_ERRORS})
+      target_compile_options(fvm_warning_flags INTERFACE -ferror-limit=${ZISA_MAX_ERRORS})
     endif()
   endif()
+else()
+  target_link_libraries(fvm_warning_flags INTERFACE warning_flags)
 endif()
 
 
