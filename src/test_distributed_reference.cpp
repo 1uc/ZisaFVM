909d3eaceeb4e1aa23f0871f47ed9dd7fb855b04
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-05 20:13:11 +0100

    Optional MPI dependency.

--- src/test_distributed_reference.cpp
+++ src/test_distributed_reference.cpp
@@ -1,3 +1,4 @@
+#if ZISA_HAS_MPI == 1
 #include <zisa/io/file_name_generator.hpp>
 #include <zisa/io/load_snapshot.hpp>
 #include <zisa/io/parallel_dump_snapshot.hpp>
@@ -219,3 +220,11 @@ int main(int argc, char *argv[]) {
 
   MPI_Finalize();
 }
+
+#else
+
+int main() {
+  LOG_ERR("This test requires `ZISA_HAS_MPI == 1`.");
+}
+
+#endif
