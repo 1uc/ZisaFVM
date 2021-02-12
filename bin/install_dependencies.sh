f9cb70e0bb3339623ff1f6166424db01d61f8430
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-12 16:36:21 +0100

    Update dependency script.

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
+set -e
+
+component_name="ZisaFVM"
 
 if [[ "$#" -lt 2 ]]
 then
     echo "Usage: $0 COMPILER DESTINATION [--zisa_has_mpi=ZISA_HAS_MPI]"
-    echo "                               [--zisa_has_metis=ZISA_HAS_METIS]"
+    echo "                               [--zisa_has_cuda=ZISA_HAS_CUDA]"
+    exit -1
 fi
 
 for arg in "$@"
@@ -12,8 +16,8 @@ do
         --zisa_has_mpi=*)
             ZISA_HAS_MPI=${arg#*=}
             ;;
-        --zisa_has_metis=*)
-            ZISA_HAS_METIS=${arg#*=}
+        --zisa_has_cuda=*)
+            ZISA_HAS_CUDA=${arg#*=}
             ;;
         *)
             ;;
@@ -25,12 +29,11 @@ then
     ZISA_HAS_MPI=0
 fi
 
-if [[ -z "${ZISA_HAS_METIS}" ]]
+if [[ -z "${ZISA_HAS_CUDA}" ]]
 then
-    ZISA_HAS_METIS=0
+    ZISA_HAS_CUDA=1
 fi
 
-component_name="ZisaFVM"
 if [[ ${ZISA_HAS_MPI} -eq 0 ]]
 then
     zisa_dependencies=("ZisaCore" "ZisaMemory" "ZisaSFC" "ZisaTimeStepping")
@@ -38,29 +41,31 @@ else
     zisa_dependencies=("ZisaCore" "ZisaMemory" "ZisaSFC" "ZisaMPI" "ZisaTimeStepping")
 fi
 
-zisa_fvm_root=$(realpath $(dirname $(readlink -f $0))/..)
 
-compiler=$1
-install_dir=$(${zisa_fvm_root}/bin/install_dir.sh $1 $2 --zisa_has_mpi=${ZISA_HAS_MPI})
-source_dir=${install_dir}/sources
-conan_file=${zisa_fvm_root}/conanfile.txt
+zisa_root="$(realpath "$(dirname "$(readlink -f "$0")")"/..)"
 
-if [[ -f $conan_file ]]
-then
-   mkdir -p ${install_dir}/conan && cd ${install_dir}/conan
-   conan install $conan_file -s compiler.libcxx=libstdc++11
-fi
+CC="$1"
+CXX="$(${zisa_root}/bin/cc2cxx.sh $CC)"
 
-mkdir -p ${source_dir}
+install_dir="$("${zisa_root}/bin/install_dir.sh" "$1" "$2" --zisa_has_mpi=${ZISA_HAS_MPI})"
+source_dir="${install_dir}/sources"
+conan_file="${zisa_root}/conanfile.txt"
+
+mkdir -p "${install_dir}/conan" && cd "${install_dir}/conan"
+conan install "$conan_file" \
+        -s compiler=$(basename "${CC}") \
+        -s compiler.libcxx=libstdc++11
+
+mkdir -p "${source_dir}"
 for dep in "${zisa_dependencies[@]}"
 do
-    src_dir=${source_dir}/$dep
+    src_dir="${source_dir}/$dep"
     repo_url=git@github.com:1uc/${dep}.git
 
     # If necessary and reasonable remove ${src_dir}.
-    if [[ -d ${src_dir} ]]
+    if [[ -d "${src_dir}" ]]
     then
-        cd ${src_dir}
+        cd "${src_dir}"
 
         if [[ -z $(git remote -v 2>/dev/null | grep ${repo_url}) ]]
         then
@@ -68,34 +73,38 @@ do
             exit -1
 
         else
-            cd ${HOME}
-            rm -rf ${src_dir}
+            cd "${HOME}"
+            rm -rf "${src_dir}"
         fi
     fi
 
-    git clone ${repo_url} ${src_dir}
+    git clone ${repo_url} "${src_dir}"
 
-    mkdir -p ${src_dir}/build-dep
-    cd ${src_dir}/build-dep
+    mkdir -p "${src_dir}/build-dep"
+    cd "${src_dir}/build-dep"
 
-    cmake -DCMAKE_INSTALL_PREFIX=${install_dir}/zisa \
-          -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \
-          -DCMAKE_MODULE_PATH=${install_dir}/conan \
-          -DCMAKE_PROJECT_${dep}_INCLUDE=${install_dir}/conan/conan_paths.cmake \
-          -DZISA_HAS_MPI=${ZISA_HAS_MPI} \
-          -DCMAKE_BUILD_TYPE=Release \
+    cmake -DCMAKE_INSTALL_PREFIX="${install_dir}/zisa" \
+          -DCMAKE_PREFIX_PATH="${install_dir}/zisa/lib/cmake/zisa" \
+          -DCMAKE_MODULE_PATH="${install_dir}/conan" \
+          -DCMAKE_PROJECT_${dep}_INCLUDE="${install_dir}/conan/conan_paths.cmake" \
+          -DCMAKE_C_COMPILER="${CC}" \
+          -DCMAKE_CXX_COMPILER="${CXX}" \
+          -DZISA_HAS_MPI="${ZISA_HAS_MPI}" \
+          -DCMAKE_BUILD_TYPE="Release" \
           ..
 
     cmake --build . --parallel $(nproc)
     cmake --install .
 done
 
-if [[ ${ZISA_HAS_METIS} -ne 0 ]]
-then
-    cd ${zisa_fvm_root}
-    bin/install_metis.sh $1
-    cd -
-fi
-
 echo "The dependencies were installed at"
-echo "    ${install_dir}"
+echo "    export DEP_DIR=${install_dir}"
+echo ""
+echo "Use"
+echo "    cmake -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \\ "
+echo "          -DCMAKE_MODULE_PATH=${install_dir}/conan \\ "
+echo "          -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \\ "
+echo "          -DCMAKE_C_COMPILER=${CC} \\ "
+echo "          -DCMAKE_CXX_COMPILER=${CXX} \\ "
+echo "          -DZISA_HAS_MPI=${ZISA_HAS_MPI} \\ "
+echo "          REMAINING_ARGS "
--- bin/install_dir.sh
+++ bin/install_dir.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+set -e
+
 if [[ "$#" -lt 2 ]]
 then
     echo "Usage: $0 COMPILER DESTINATION [--zisa_has_mpi=ZISA_HAS_MPI]"
+    echo "                               [--zisa_has_cuda=ZISA_HAS_CUDA]"
+    exit -1
 fi
 
 for arg in "$@"
@@ -10,6 +14,9 @@ do
     case $arg in
         --zisa_has_mpi=*)
             ZISA_HAS_MPI=${arg#*=}
+            ;;
+        --zisa_has_cuda=*)
+            ZISA_HAS_CUDA=${arg#*=}
             ;;
         *)
             ;;
@@ -21,15 +28,19 @@ then
     ZISA_HAS_MPI=0
 fi
 
-component_name="ZisaFVM"
+if [[ -z "${ZISA_HAS_CUDA}" ]]
+then
+    ZISA_HAS_CUDA=0
+fi
 
-compiler=$1
-compiler_id=$(basename ${compiler})
-compiler_version=$($compiler -dumpversion)
+compiler="$1"
+compiler_id="$(basename "${compiler}")"
+compiler_version="$("$compiler" -dumpversion)"
 
-read -r build_sha junk <<< "$(echo "${compiler}__${compiler_version}__ZISA_HAS_MPI=${ZISA_HAS_MPI}" | sha1sum)"
+build_mangled_name="${compiler}__${compiler_version}__ZISA_HAS_MPI=${ZISA_HAS_MPI}__ZISA_HAS_CUDA=${ZISA_HAS_CUDA}"
+build_sha="$(echo "${build_mangled_name}" | sha1sum | head -c 6)"
 
-install_root=$2
-install_dir=${install_root}/${component_name}/${build_sha}
+install_root="${PWD}/$2"
+install_dir="${install_root}/${build_sha}"
 
-echo ${install_dir}
+echo "${install_dir}"
