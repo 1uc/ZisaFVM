8ff93678e9a569dd0e0428a8dad65da7768b25f4
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-23 16:49:44 +0100

    Improve dependency scripts.

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -7,6 +7,7 @@ if [[ "$#" -lt 2 ]]
 then
     echo "Usage: $0 COMPILER DESTINATION [--zisa_has_mpi=ZISA_HAS_MPI]"
     echo "                               [--zisa_has_cuda=ZISA_HAS_CUDA]"
+    echo "                               [--cmake=CUSTOM_CMAKE_BINARY]"
     exit -1
 fi
 
@@ -19,10 +20,18 @@ do
         --zisa_has_cuda=*)
             ZISA_HAS_CUDA=${arg#*=}
             ;;
+        --cmake=*)
+            CMAKE=$(realpath ${arg#*=})
+            ;;
         *)
             ;;
     esac
 done
+
+if [[ -z "${CMAKE}" ]]
+then
+    CMAKE=cmake
+fi
 
 if [[ -z "${ZISA_HAS_MPI}" ]]
 then
@@ -83,18 +92,18 @@ do
     mkdir -p "${src_dir}/build-dep"
     cd "${src_dir}/build-dep"
 
-    cmake -DCMAKE_INSTALL_PREFIX="${install_dir}/zisa" \
-          -DCMAKE_PREFIX_PATH="${install_dir}/zisa/lib/cmake/zisa" \
-          -DCMAKE_MODULE_PATH="${install_dir}/conan" \
-          -DCMAKE_PROJECT_${dep}_INCLUDE="${install_dir}/conan/conan_paths.cmake" \
-          -DCMAKE_C_COMPILER="${CC}" \
-          -DCMAKE_CXX_COMPILER="${CXX}" \
-          -DZISA_HAS_MPI="${ZISA_HAS_MPI}" \
-          -DCMAKE_BUILD_TYPE="Release" \
-          ..
+    ${CMAKE} -DCMAKE_INSTALL_PREFIX="${install_dir}/zisa" \
+             -DCMAKE_PREFIX_PATH="${install_dir}/zisa/lib/cmake/zisa" \
+             -DCMAKE_MODULE_PATH="${install_dir}/conan" \
+             -DCMAKE_PROJECT_${dep}_INCLUDE="${install_dir}/conan/conan_paths.cmake" \
+             -DCMAKE_C_COMPILER="${CC}" \
+             -DCMAKE_CXX_COMPILER="${CXX}" \
+             -DZISA_HAS_MPI="${ZISA_HAS_MPI}" \
+             -DCMAKE_BUILD_TYPE="Release" \
+             ..
 
-    cmake --build . --parallel $(nproc)
-    cmake --install .
+    ${CMAKE} --build . --parallel $(nproc)
+    ${CMAKE} --install .
 done
 
 if [[ ${ZISA_HAS_METIS} -ne 0 ]]
@@ -109,10 +118,10 @@ echo "The dependencies were installed at"
 echo "    export DEP_DIR=${install_dir}"
 echo ""
 echo "Use"
-echo "    cmake -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \\ "
-echo "          -DCMAKE_MODULE_PATH=${install_dir}/conan \\ "
-echo "          -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \\ "
-echo "          -DCMAKE_C_COMPILER=${CC} \\ "
-echo "          -DCMAKE_CXX_COMPILER=${CXX} \\ "
-echo "          -DZISA_HAS_MPI=${ZISA_HAS_MPI} \\ "
-echo "          REMAINING_ARGS "
+echo "    ${CMAKE} -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \\ "
+echo "             -DCMAKE_MODULE_PATH=${install_dir}/conan \\ "
+echo "             -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \\ "
+echo "             -DCMAKE_C_COMPILER=${CC} \\ "
+echo "             -DCMAKE_CXX_COMPILER=${CXX} \\ "
+echo "             -DZISA_HAS_MPI=${ZISA_HAS_MPI} \\ "
+echo "             REMAINING_ARGS "
