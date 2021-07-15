d6a44f1ec6d4d15d04760902c126fac63de42440
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-07-15 10:53:22 +0200

    Improve install script.

--- bin/install_cmake.sh
+++ bin/install_cmake.sh
@@ -8,23 +8,26 @@ if [[ $# -ne 1 ]]; then
   exit -1
 fi
 
-INSTALL_PREFIX=$1
+INSTALL_PREFIX=$(realpath $1)
 
 CMAKE_MAJOR_VERSION=3
-CMAKE_MINOR_VERSION=13
+CMAKE_MINOR_VERSION=19
 CMAKE_RELEASE_VERSION=4
 
-TMP_DIR=$(mktemp -d --tmpdir=${SCRATCH})
-CMAKE_DIR="cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_RELEASE_VERSION}"
+CMAKE_VERSION="${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}"
+CMAKE_FULL_VERSION="${CMAKE_VERSION}.${CMAKE_RELEASE_VERSION}"
+
+TMP_DIR=$(realpath "$(mktemp -d --tmpdir=.)")
+CMAKE_DIR="cmake-${CMAKE_FULL_VERSION}"
 
 cd $TMP_DIR
-wget https://cmake.org/files/v${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_RELEASE_VERSION}.tar.gz
-tar xvf cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_RELEASE_VERSION}.tar.gz
+wget https://cmake.org/files/v${CMAKE_VERSION}/cmake-${CMAKE_FULL_VERSION}.tar.gz
+tar xvf cmake-${CMAKE_FULL_VERSION}.tar.gz
 
-cd ${CMAKE_DIR}
-./bootstrap --system-curl --prefix=$INSTALL_PREFIX
+cd "${CMAKE_DIR}"
+./bootstrap --system-curl --prefix="$INSTALL_PREFIX"
 make -j$(nproc)
 make install
 
-cd ${HOME}
-rm -rf $TMP_DIR
+cd "${HOME}"
+rm -rf "$TMP_DIR"
